0d7eafae65fadfde0e2385faac8c55acc743e64f
Bhavay37 <pantbhavay56@gmail.com>
2018-08-19 04:45:21 +0530

    This is a new commit to dev only

--- Research.Innovation.CaseStudy/src/test/java/AutomationFramework/YourLogoAutomation.java
+++ Research.Innovation.CaseStudy/src/test/java/AutomationFramework/YourLogoAutomation.java
@@ -21,7 +21,7 @@ public class YourLogoAutomation {
 	
 	public static WebDriver driver= null;
 	Properties prop;
-	// this is a new commit to dev branch
+	// this is a second commit to dev branch
 	@BeforeTest
 	public void StartUp()throws Exception{
 		
